bc214d6ffba10a0dd7e782e7364cdac0bc027538
ofekeliyahu <104149847+ofekeliyahu@users.noreply.github.com>
2023-05-16 23:54:36 +0300

    Add files via upload

--- java/scripts.js
+++ java/scripts.js
@@ -33,5 +33,63 @@ function changebackgrund(){
                 document.getElementById("copsEvent").append(newCop);
                  }
       
-              
+             $('#submitBtn').click(function() {
+        checked = document.getElementsByName("interests[]");
+        var list = $("input[name='interests[]']:checked").map(function () {
+          return this.value;
+            }).get();
+            alert("תגבור נשלח ל " + list);
+              $('#reinforcementsModal').modal('toggle');
+           
       });
+       
+      $('#Btn_fireman_req').click(function() {
+      
+            alert("כיבוי אש הוזמן למיקום ");
+           
+      });
+      $('#Btn_medical_req').click(function() {
+      
+        alert("מדא הוזמנה למיקום");
+       
+      });
+      
+      
+      $("#checkbox_checkAll").click(function() {
+      $("#checkbox_hashalom").attr('disabled', !$("#checkbox_hashalom").attr('disabled'));
+      $("#checkbox_aria").attr('disabled', !$("#checkbox_aria").attr('disabled'));
+      $("#reinforcement_yorashalayem").attr('disabled', !$("#reinforcement_yorashalayem").attr('disabled'));
+        });
+      
+      
+      });
+
+
+
+      function sub(){
+        document.getElementById("btnform").onclick = function () {
+        let temp=document.getElementsByName('actname')[0].value;
+            if (temp=="") {
+                document.getElementById("name").classList.add("is-invalid")
+             }
+             let temp1=document.getElementsByName('date')[0].value;
+            if (temp1=="") {
+                document.getElementById("date").classList.add("is-invalid")
+             }
+             let temp2=document.getElementsByName('loc')[0].value;
+             if (temp2=="") {
+                 document.getElementById("loc").classList.add("is-invalid")
+              }
+              let temp3=document.getElementsByName('officer')[0].value;
+              if (temp3=="") {
+                  document.getElementById("officer").classList.add("is-invalid")
+               }
+               let temp4=document.getElementsByName('force')[0].value;
+              if (temp4=="") {
+                  document.getElementById("force").classList.add("is-invalid")
+               }
+        }
+    
+    }
+
+
