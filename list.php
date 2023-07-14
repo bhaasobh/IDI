e6e7c820c9d2eebc5056787fcfdd2fa65d0b2ee0
ofekeliyahu <104149847+ofekeliyahu@users.noreply.github.com>
2023-07-14 11:54:51 +0300

    Add files via upload

--- list.php
+++ list.php
@@ -11,10 +11,13 @@ session_start();
     } else {
       //  echo 'user id exists: ' . $_SESSION["user_id"] ;
         $userID = $_SESSION["user_id"] ;
-        $query  = "SELECT * FROM tbl_206_users WHERE id=" . $userID;
+        $query  = "SELECT * FROM tbl_206_events";
+        $query1  = "SELECT * FROM tbl_206_users WHERE id=" . $userID;
       // echo $query;
         $result = mysqli_query($connection , $query);
         $row    = mysqli_fetch_array($result);
+        $result1 = mysqli_query($connection , $query1);
+        $row1    = mysqli_fetch_array($result1);
     }
    //$model = mysqli_query($connection, $query);
    //if(!$model) {
@@ -122,10 +125,10 @@ session_start();
         </a>
       </div>
       <div class="p-2">
-        <h6 id="profileName">שלום  <?php echo $row["Rank"]. " ". $row["name"] ?></h6>
+        <h6 id="profileName">שלום  <?php echo $row1["Rank"]. " ". $row1["name"] ?></h6>
       </div>
       <div class="p-2">
-        <img src="<?php echo $row["photo_path"] ?>" class="profilePic" alt="danit" title="danit">
+        <img src="<?php echo $row1["photo_path"] ?>" class="profilePic" alt="danit" title="danit">
       </div>
     </div>
   </header>
@@ -165,198 +168,56 @@ session_start();
         </div>
         <div id="boxs">
           <ul>
-            <li>
-              <a href="event.html?eventId=1">
-                <div class="card">
-    
-                    <div class="card-body">
-                        <div class="row">
-                           <div class="col-9">
-                             <h4 class="card-title">הפגנה בהבימה תל אביב</h4>
-                             <h5 class="card-subtitle mb-2 text-muted">שוטרים פעילים:8</h5>
-                             <h5 class="card-subtitle mb-2 text-muted">רמת סיכון:גבוהה</h5>
-                            </div>
-                            <div class="col-3">
-                              <p class="date-and-time">
-                                12/05/2023
-                                <br>
-                                20:00
-                              </p>
-                              <div class="icons">
-                                
-                                  <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
-                                    class="bi bi-trash" viewBox="0 0 16 16">
-                                    <path
-                                    d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z" />
-                                    <path
-                                    d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z" />
-                                  </svg>
-                                
-                                
-                                  <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
-                                  class="bi bi-pencil" viewBox="0 0 16 16">
-                                  <path
-                                  d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
-                                </svg>
-                                
-                               
-                                  <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
-                                    class="bi bi-star" viewBox="0 0 16 16">
-                                    <path
-                                      d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
-                                    </svg>
-                                  
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                  </div>
-                </a>
-              </li>
-        <li>
-          <a href="event.html?eventId=2">
-            <div class="card">
+          <?php
+            while($row = mysqli_fetch_assoc($result)){
+              echo    '<li>';
+              echo    	'<a href="event.php?eventId=' . $row["event_id"] . '"</a>';
+              echo        '<div class="card">';
+              echo          '<div class="card-body">';
+              echo             '<div class="row">';
+              echo                '<div class="col-9">';
+              echo                '<h4 class="card-title">'.$row["title"].'</h4>';
+              echo                '<h5 class="card-subtitle mb-2 text-muted">שוטרים פעילים:'.$row["officer_quantity"].'</h5>';
+              echo                 '<h5 class="card-subtitle mb-2 text-muted">רמת סיכון:'.$row["risk_level"].'</h5>';
+              echo                  '</div>';
+              echo                   '<div class="col-3">';
+              echo                    ' <p class="date-and-time">'
+                                         .$row["date"].
+                                         '<br>'
+                                       .$row["time_start"].
+                                        '</p>';
+              echo                      '<div class="icons">';
+              echo                        '<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
+                                            class="bi bi-trash" viewBox="0 0 16 16">
+                                            <path
+                                            d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z" />
+                                            <path
+                                            d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z" />
+                                          </svg>
+                                        
+                                        
+                                          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
+                                          class="bi bi-pencil" viewBox="0 0 16 16">
+                                          <path
+                                          d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
+                                        </svg>
+                                        
+                                      
+                                          <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
+                                            class="bi bi-star" viewBox="0 0 16 16">
+                                            <path
+                                              d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
+                                            </svg>';
+            echo                      '</div>' ;
+            echo                    '</div>' ;
+            echo                   '</div>'  ;
+            echo                 '</div>' ;
+            echo               '</div>' ;
+            echo            '</a>' ;
+            echo          '</li>' ;
 
-                <div class="card-body">
-                    <div class="row">
-                       <div class="col-9">
-                         <h4 class="card-title">הפגנה בבית רה"מ ירושלים</h4>
-                         <h5 class="card-subtitle mb-2 text-muted">שוטרים פעילים:20</h5>
-                         <h5 class="card-subtitle mb-2 text-muted">רמת סיכון:גבוהה</h5>
-                        </div>
-                        <div class="col-3">
-                          <p class="date-and-time">
-                            13/05/2023
-                            <br>
-                            20:00
-                          </p>
-                          <div class="icons">
-                            
-                              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
-                                class="bi bi-trash" viewBox="0 0 16 16">
-                                <path
-                                d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z" />
-                                <path
-                                d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z" />
-                              </svg>
-                            
-                            
-                              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
-                              class="bi bi-pencil" viewBox="0 0 16 16">
-                              <path
-                              d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
-                            </svg>
-                            
-                           
-                              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
-                                class="bi bi-star" viewBox="0 0 16 16">
-                                <path
-                                  d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
-                                </svg>
-                              
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-              </div>
-            </a>
-          </li>
-        <li>
-          <a href="event.html?eventId=3">
-            <div class="card">
-
-                <div class="card-body">
-                    <div class="row">
-                       <div class="col-9">
-                         <h4 class="card-title">הפגנה בהר הבית ירושלים</h4>
-                         <h5 class="card-subtitle mb-2 text-muted">שוטרים פעילים:30</h5>
-                         <h5 class="card-subtitle mb-2 text-muted">רמת סיכון:גבוהה</h5>
-                        </div>
-                        <div class="col-3">
-                          <p class="date-and-time">
-                            13/05/2023
-                            <br>
-                            21:00
-                          </p>
-                          <div class="icons">
-                            
-                              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
-                                class="bi bi-trash" viewBox="0 0 16 16">
-                                <path
-                                d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z" />
-                                <path
-                                d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z" />
-                              </svg>
-                            
-                            
-                              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
-                              class="bi bi-pencil" viewBox="0 0 16 16">
-                              <path
-                              d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
-                            </svg>
-                            
-                           
-                              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
-                                class="bi bi-star" viewBox="0 0 16 16">
-                                <path
-                                  d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
-                                </svg>
-                              
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-              </div>
-            </a>
-          </li>
-        <li>
-          <a href="event.html?eventId=4">
-            <div class="card">
-
-                <div class="card-body">
-                    <div class="row">
-                       <div class="col-9">
-                         <h4 class="card-title">הפגנה ברחבת חורב חיפה</h4>
-                         <h5 class="card-subtitle mb-2 text-muted">שוטרים פעילים:7</h5>
-                         <h5 class="card-subtitle mb-2 text-muted">רמת סיכון:בינונית</h5>
-                        </div>
-                        <div class="col-3">
-                          <p class="date-and-time">
-                            14/05/2023
-                            <br>
-                            20:00
-                          </p>
-                          <div class="icons">
-                            
-                              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
-                                class="bi bi-trash" viewBox="0 0 16 16">
-                                <path
-                                d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z" />
-                                <path
-                                d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z" />
-                              </svg>
-                            
-                            
-                              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
-                              class="bi bi-pencil" viewBox="0 0 16 16">
-                              <path
-                              d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
-                            </svg>
-                            
-                           
-                              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
-                                class="bi bi-star" viewBox="0 0 16 16">
-                                <path
-                                  d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
-                                </svg>
-                              
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-              </div>
-            </a>
-          </li>
+            }
+            ?>   
         </ul>
         </div>
         <a type="button" class="btn btn-secondary" id="add-event" href="form.html">
@@ -424,5 +285,7 @@ session_start();
   </section>
   <script>changebackgrund()</script>
 </body>
-
 </html>
+<?php
+    mysqli_close($connection);
+?>
